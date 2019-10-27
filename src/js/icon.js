0522ac60fd64dd6b43a25e9754d7d7b61291cc93
cutelee <soyoung.dev@gmail.com>
2019-10-27 18:39:54 +0900

    삭제, 즐겨찾기 아이콘 및 접기 기능 추가

---
+++ src/js/icon.js
@@ -0,0 +1,320 @@
+function getTrashSVG() {
+  return `<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+\t viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+<g>
+\t<g>
+\t\t<path d="M465.423,48.241h-137.61V23.955C327.813,10.746,317.082,0,303.893,0h-95.785c-13.19,0-23.92,10.746-23.92,23.955V48.24
+\t\t\tH46.577c-6.655,0-12.049,5.394-12.049,12.049c0,6.655,5.394,12.049,12.049,12.049h22.332l15.228,396.396
+\t\t\tC85.069,492.995,104.818,512,129.099,512h253.804c24.281,0,44.03-19.006,44.96-43.267l15.228-396.396h22.332
+\t\t\tc6.653,0,12.049-5.394,12.049-12.049C477.472,53.635,472.078,48.241,465.423,48.241z M208.285,24.097h95.43v24.143h-95.43V24.097z
+\t\t\t M403.784,467.809c-0.433,11.268-9.605,20.094-20.882,20.094H129.099c-11.276,0-20.448-8.827-20.882-20.095L93.025,72.338h325.952
+\t\t\tL403.784,467.809z"/>
+\t</g>
+</g>
+<g>
+\t<g>
+\t\t<path d="M182.63,181.571c-0.127-6.575-5.494-11.817-12.042-11.817c-0.078,0-0.158,0-0.236,0.002
+\t\t\tc-6.652,0.128-11.943,5.626-11.815,12.278l3.781,196.634c0.126,6.575,5.495,11.817,12.042,11.817c0.078,0,0.158,0,0.236-0.002
+\t\t\tc6.653-0.128,11.943-5.624,11.815-12.278L182.63,181.571z"/>
+\t</g>
+</g>
+<g>
+\t<g>
+\t\t<path d="M255.998,169.753c-6.654,0-12.049,5.394-12.049,12.049v196.634c0,6.654,5.394,12.049,12.049,12.049
+\t\t\tc6.655,0,12.049-5.394,12.049-12.049V181.802C268.047,175.148,262.653,169.753,255.998,169.753z"/>
+\t</g>
+</g>
+<g>
+\t<g>
+\t\t<path d="M341.645,169.756c-6.628-0.147-12.151,5.162-12.278,11.815l-3.781,196.634c-0.129,6.653,5.162,12.15,11.815,12.278
+\t\t\tc0.078,0.001,0.158,0.002,0.236,0.002c6.546,0,11.916-5.244,12.042-11.817l3.781-196.634
+\t\t\tC353.588,175.38,348.299,169.883,341.645,169.756z"/>
+\t</g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+</svg>`;
+}
+function getStarSVG() {
+  return `<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+\t viewBox="0 0 49.94 49.94" style="enable-background:new 0 0 49.94 49.94;" xml:space="preserve">
+            <path d="M48.856,22.731c0.983-0.958,1.33-2.364,0.906-3.671c-0.425-1.307-1.532-2.24-2.892-2.438l-12.092-1.757
+            \tc-0.515-0.075-0.96-0.398-1.19-0.865L28.182,3.043c-0.607-1.231-1.839-1.996-3.212-1.996c-1.372,0-2.604,0.765-3.211,1.996
+            \tL16.352,14c-0.23,0.467-0.676,0.79-1.191,0.865L3.069,16.623C1.71,16.82,0.603,17.753,0.178,19.06
+            \tc-0.424,1.307-0.077,2.713,0.906,3.671l8.749,8.528c0.373,0.364,0.544,0.888,0.456,1.4L8.224,44.702
+            \tc-0.232,1.353,0.313,2.694,1.424,3.502c1.11,0.809,2.555,0.914,3.772,0.273l10.814-5.686c0.461-0.242,1.011-0.242,1.472,0
+            \tl10.815,5.686c0.528,0.278,1.1,0.415,1.669,0.415c0.739,0,1.475-0.231,2.103-0.688c1.111-0.808,1.656-2.149,1.424-3.502
+            \tL39.651,32.66c-0.088-0.513,0.083-1.036,0.456-1.4L48.856,22.731z M37.681,32.998l2.065,12.042c0.104,0.606-0.131,1.185-0.629,1.547
+            \tc-0.499,0.361-1.12,0.405-1.665,0.121l-10.815-5.687c-0.521-0.273-1.095-0.411-1.667-0.411s-1.145,0.138-1.667,0.412l-10.813,5.686
+            \tc-0.547,0.284-1.168,0.24-1.666-0.121c-0.498-0.362-0.732-0.94-0.629-1.547l2.065-12.042c0.199-1.162-0.186-2.348-1.03-3.17
+            \tL2.48,21.299c-0.441-0.43-0.591-1.036-0.4-1.621c0.19-0.586,0.667-0.988,1.276-1.077l12.091-1.757
+            \tc1.167-0.169,2.176-0.901,2.697-1.959l5.407-10.957c0.272-0.552,0.803-0.881,1.418-0.881c0.616,0,1.146,0.329,1.419,0.881
+            \tl5.407,10.957c0.521,1.058,1.529,1.79,2.696,1.959l12.092,1.757c0.609,0.089,1.086,0.491,1.276,1.077
+            \tc0.19,0.585,0.041,1.191-0.4,1.621l-8.749,8.528C37.866,30.65,37.481,31.835,37.681,32.998z"/>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            <g>
+            </g>
+            </svg>`;
+}
+function getCrossRemoveSVG() {
+  return `<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+\t width="95.939px" height="95.939px" viewBox="0 0 95.939 95.939" style="enable-background:new 0 0 95.939 95.939;"
+\t xml:space="preserve">
+<g>
+\t<path d="M62.819,47.97l32.533-32.534c0.781-0.781,0.781-2.047,0-2.828L83.333,0.586C82.958,0.211,82.448,0,81.919,0
+\t\tc-0.53,0-1.039,0.211-1.414,0.586L47.97,33.121L15.435,0.586c-0.75-0.75-2.078-0.75-2.828,0L0.587,12.608
+\t\tc-0.781,0.781-0.781,2.047,0,2.828L33.121,47.97L0.587,80.504c-0.781,0.781-0.781,2.047,0,2.828l12.02,12.021
+\t\tc0.375,0.375,0.884,0.586,1.414,0.586c0.53,0,1.039-0.211,1.414-0.586L47.97,62.818l32.535,32.535
+\t\tc0.375,0.375,0.884,0.586,1.414,0.586c0.529,0,1.039-0.211,1.414-0.586l12.02-12.021c0.781-0.781,0.781-2.048,0-2.828L62.819,47.97
+\t\tz"/>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+</svg>`;
+}
+function getDownArrowSVG() {
+  return `<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+\t width="444.819px" height="444.819px" viewBox="0 0 444.819 444.819" style="enable-background:new 0 0 444.819 444.819;"
+\t xml:space="preserve">
+<g>
+\t<path d="M434.252,114.203l-21.409-21.416c-7.419-7.04-16.084-10.561-25.975-10.561c-10.095,0-18.657,3.521-25.7,10.561
+\t\tL222.41,231.549L83.653,92.791c-7.042-7.04-15.606-10.561-25.697-10.561c-9.896,0-18.559,3.521-25.979,10.561l-21.128,21.416
+\t\tC3.615,121.436,0,130.099,0,140.188c0,10.277,3.619,18.842,10.848,25.693l185.864,185.865c6.855,7.23,15.416,10.848,25.697,10.848
+\t\tc10.088,0,18.75-3.617,25.977-10.848l185.865-185.865c7.043-7.044,10.567-15.608,10.567-25.693
+\t\tC444.819,130.287,441.295,121.629,434.252,114.203z"/>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+</svg>`;
+}
+function getUpArrowSVG() {
+  return `
+  <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+\t width="444.819px" height="444.819px" viewBox="0 0 444.819 444.819" style="enable-background:new 0 0 444.819 444.819;"
+\t xml:space="preserve">
+<g>
+\t<path d="M433.968,278.657L248.387,92.79c-7.419-7.044-16.08-10.566-25.977-10.566c-10.088,0-18.652,3.521-25.697,10.566
+\t\tL10.848,278.657C3.615,285.887,0,294.549,0,304.637c0,10.28,3.619,18.843,10.848,25.693l21.411,21.413
+\t\tc6.854,7.23,15.42,10.852,25.697,10.852c10.278,0,18.842-3.621,25.697-10.852L222.41,213.271L361.168,351.74
+\t\tc6.848,7.228,15.413,10.852,25.7,10.852c10.082,0,18.747-3.624,25.975-10.852l21.409-21.412
+\t\tc7.043-7.043,10.567-15.608,10.567-25.693C444.819,294.545,441.205,285.884,433.968,278.657z"/>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+<g>
+</g>
+</svg>`;
+}
+
+window.getRecentLinkHTML = (title, link) => {
+  return `
+    <li class="link-item">
+      <a class="link-item-title" href=${link}>${title}</a>
+      <div class="link-item-button">
+        <span class="link-item-delete-button">
+          ${getTrashSVG()}
+        </span>
+        <span class="link-item-favorite-button">
+          ${getStarSVG()}
+        </span>
+      </div>
+    </li>
+  `;
+};
+
+window.getKeywordItemHTML = (keyword) => {
+  const liEl = document.createElement('li');
+  liEl.className = 'keyword-item';
+  liEl.innerHTML = `
+       <div class="keyword-item-title">
+         <span>${keyword.keyword}</span>
+         <img alt="naver-icon" class="keyword-item-search" src="../images/naver.png">
+         <img alt="google-icon" class="keyword-item-search" src="../images/google.png">
+         <img alt="youtube-icon" class="keyword-item-search" src="../images/youtube.png">
+         <span class="keyword-item-delete">${getCrossRemoveSVG()}</span>
+         <span class="keyword-item-show-more">${getDownArrowSVG()}</span>
+       </div>
+       <div class="keyword-item-content-hidden">
+          <h4 class="link-type-info">즐겨찾는 검색결과</h4>
+         <ul class="link-favorite-items">
+           ${keyword.favoriteLink.map(link => window.getRecentLinkHTML(link.title, link.link)).join('')}
+         </ul>
+         <h4 class="link-type-info" style="margin-top: 20px;">최근 방문한 검색결과</h4>
+         <ul class="link-recent-items">
+           ${keyword.recentLink.map(link => window.getRecentLinkHTML(link.title, link.link)).join('')}
+         </ul>
+      </div>
+     `;
+  return liEl;
+};
+
+window.onClickUpArrow = (e) => {
+  let spanNode = e.target.parentNode;
+  if (spanNode.tagName === 'g') {
+    spanNode = spanNode.parentNode.parentNode;
+  }
+
+  const parentNode = spanNode.parentNode;
+  parentNode.removeChild(spanNode);
+  const UpEl = document.createElement("span");
+  UpEl.className = 'keyword-item-show-more';
+  UpEl.addEventListener('click', onClickDownArrow);
+  UpEl.innerHTML = getDownArrowSVG();
+
+  parentNode.appendChild(UpEl);
+  parentNode.parentNode.querySelector('.keyword-item-content').className = 'keyword-item-content-hidden';
+};
+
+window.onClickDownArrow = (e) => {
+  let spanNode = e.target.parentNode;
+  if (spanNode.tagName === 'g') {
+    spanNode = spanNode.parentNode.parentNode;
+  }
+
+  const parentNode = spanNode.parentNode;
+  parentNode.removeChild(spanNode);
+  const UpEl = document.createElement("span");
+  UpEl.className = 'keyword-item-show-more';
+  UpEl.addEventListener('click', onClickUpArrow);
+  UpEl.innerHTML = getUpArrowSVG();
+
+  parentNode.appendChild(UpEl);
+  parentNode.parentNode.querySelector('.keyword-item-content-hidden').className = 'keyword-item-content';
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 const keywords = [{
   keyword: '사과',
-  favoriteLink: [{ title: '사과 - 위키백과', link: '#' }, { title: '사과 - 위키백과', link: '#' }],
+  favoriteLink: [{ title: '사과 - 위키백과위키백과위키백과위키백과위위키백과', link: '#' }, { title: '사과 - 위키백과', link: '#' }],
   recentLink: [{ title: '사과2 - 위키백과', link: '#' }, { title: '사과2 - 위키백과', link: '#' }, { title: '사과2 - 위키백과', link: '#' }],
 }, {
   keyword: '포도가 맛있는 곳',
@@ -8,46 +8,11 @@ const keywords = [{
   recentLink: [{ title: '초록색 포도 - 위키백과', link: '#' }],
 }];
 
-function getRecentLinkHTML(title, link) {
-  return `
-    <li class="link-item">
-      <a class="link-item-title" href=${link}>${title}</a>
-      <div class="link-item-button">
-        <button>삭제</button>
-        <button>즐겨찾기</button>
-      </div>
-    </li>
-  `;
-}
-
-function getKeywordItemHTML(keyword) {
-  const liEl = document.createElement('li');
-  liEl.className = 'keyword-item';
-  liEl.innerHTML = `
-       <div class="keyword-item-title">
-         <span>${keyword.keyword}</span>
-         <button class="keyword-item-delete">삭제</button>
-         <img alt="naver-icon" class="keyword-item-search" src="../images/naver.png">
-         <img alt="google-icon" class="keyword-item-search" src="../images/google.png">
-         <img alt="youtube-icon" class="keyword-item-search" src="../images/youtube.png">
-       </div>
-       <h4 class="link-type-info">즐겨찾는 검색결과</h4>
-       <ul class="link-favorite-items">
-         ${keyword.favoriteLink.map(link => getRecentLinkHTML(link.title, link.link)).join('')}
-       </ul>
-       <h4 class="link-type-info" style="margin-top: 20px;">최근 방문한 검색결과</h4>
-       <ul class="link-recent-items">
-         ${keyword.recentLink.map(link => getRecentLinkHTML(link.title, link.link)).join('')}
-       </ul>
-     `;
-
-  return liEl;
-}
-
 (function() {
  const keywordItemsEl = document.querySelector('.keyword-items');
  keywords.forEach(keyword => {
    const keywordEl = getKeywordItemHTML(keyword);
-   keywordItemsEl.appendChild(keywordEl);
+   const result = keywordItemsEl.appendChild(keywordEl);
+   result.querySelector('.keyword-item-show-more').addEventListener('click', window.onClickDownArrow);
  });
 })();
