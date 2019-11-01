9a48e0962026bfe31991242c47769addfe192729
cutelee <soyoung.dev@gmail.com>
2019-11-01 18:12:14 +0900

    네이버 블로그에서 키워드 저장모드로 아이템 선택 시 storage에 저장하는 기능 추가

---
+++ src/background/background.js
@@ -0,0 +1,22 @@
+whale.runtime.onMessage.addListener((msg, sender, sendRes) => {
+  if (msg.type === 'UPDATE_KEYWORDS') {
+    whale.storage.sync.set({ keywords: msg.payload });
+  }
+  else if (msg.type === 'ADD_LINK_TO_KEYWORD') {
+    const { keyword, link } = msg.payload;
+    whale.storage.sync.get(['keywords'], ({ keywords }) => {
+      // TODO: 중복검사
+
+      // 저장
+      whale.storage.sync.set({
+        keywords: {
+          ...keywords,
+          [keyword]: {
+            ...keywords[keyword],
+            link: keywords[keyword].link.concat(link),
+          }
+        }
+      });
+    });
+  }
+});
