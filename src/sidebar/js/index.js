9baa0ecb11b1600c0ff69ff3d1793c3f9f8a57e6
cutelee <soyoung.dev@gmail.com>
2019-11-04 18:52:21 +0900

    정렬 및 사이드바 새로고침 후 발생하는 이슈 해결

--- src/sidebar/js/index.js
+++ src/sidebar/js/index.js
@@ -1,32 +1,35 @@
-// const keywords = [ {
-//   keyword: '사과',
-//   favoriteLink: [ { title: '사과 - 위키백과위키백과위키백과위키백과위위키백과', link: '#' }, { title: '사과 - 위키백과', link: '#' } ],
-//   recentLink: [ { title: '사과2 - 위키백과', link: '#' }, { title: '사과2 - 위키백과', link: '#' }, {
-//     title: '사과2 - 위키백과',
-//     link: '#'
-//   } ],
-// }, {
-//   keyword: '포도가 맛있는 곳',
-//   favoriteLink: [ { title: '집포도 - 위키백과', link: '#' } ],
-//   recentLink: [ { title: '초록색 포도 - 위키백과', link: '#' } ],
-// } ];
+// 스토리지 초기화 하기
+const ulEl = document.querySelector('.keyword-items-list');
+
+window.initKeywordListByType = (type, keywords, keywordsOrder) => {
+  // 최신순/이름순 클릭했을 때를 위하여 초기화
+  ulEl.innerHTML = '';
+
+  if (type === 'recent') {
+    keywordsOrder.reverse().forEach(keywordName => {
+      ulEl.appendChild(getKeywordItemHTML(keywordName, keywords[keywordName]));
+    });
+  } else {
+    [...Object.keys(keywords)].forEach(keywordName => {
+      ulEl.appendChild(getKeywordItemHTML(keywordName, keywords[keywordName]));
+    });
+  }
+};
 
 (function () {
-  whale.storage.sync.get(['keywords'], ({ keywords }) => {
+  window.orderState = 'recent';   // recent, name
+  whale.storage.sync.get(['keywords', 'keywordsOrder'], ({ keywords, keywordsOrder }) => {
     if (!keywords) {
-      // 스토리지 초기화 하기
       return;
     }
-    const ulEl = document.querySelector('.keyword-items-list');
-    [...Object.keys(keywords)].forEach(keywordName => {
-      ulEl.appendChild(window.getKeywordItemHTML(keywordName, keywords[keywordName]));
-    });
-
-    // TODO: 이벤트 리스너 달기
+    window.initKeywordListByType(window.orderState, keywords, keywordsOrder);
   });
 
   // 검색창 onchange 핸들러 추가
   document.querySelector('.search-input').addEventListener('keypress', window.handleSearchKeyword);
+  document.querySelectorAll('.sort-button').forEach(sortButtonEl => {
+    sortButtonEl.addEventListener('click', window.handleClickSortButton);
+  });
 
   // storage 변화 감지하여 사이드바 화면 업데이트
   whale.runtime.onMessage.addListener((msg, sender, sendRes) => {
@@ -42,7 +45,7 @@
     if (keywordsOrder) {
       const { newValue, oldValue } = keywordsOrder;
       if (newValue.length > oldValue.length) {
-        addKeywordLiToList(newValue[newValue.length - 1]);
+        ulEl.appendChild(getKeywordItemHTML(newValue[newValue.length - 1]));
       }
     }
   });
