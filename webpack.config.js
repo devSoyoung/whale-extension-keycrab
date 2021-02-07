21200c849732f16ccba726e685cb130e76e4e779
SoYoung Lee <soyoung.dev@gmail.com>
2021-02-07 19:56:45 +0900

    content-script - common, google, naver 각각 entry 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
 const path = require('path');
 
 const srcPath = path.resolve(__dirname, "src");
+const contentScriptPath = path.join(srcPath, "content-script");
 
 module.exports = {
   entry: {
-    background: path.join(srcPath, "background/index.js")
+    background: path.join(srcPath, "background/index.js"),
+    "content-script/common": path.join(contentScriptPath, "common.js"),
+    "content-script/google": path.join(contentScriptPath, "google/google-unified-search.js"),
+    "content-script/naver": path.join(contentScriptPath, "naver/index.js")
   },
   output: {
     path: path.resolve(__dirname, "dist"),
