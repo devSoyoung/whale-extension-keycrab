ea9036b60c65f2be56892d0101bd590a0e970ae9
jincy7 <jincy713@gmail.com>
2021-04-11 21:27:50 +0900

    feat: ListView 영역 구현

--- src/sidebar-react/App.tsx
+++ src/sidebar-react/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "./components/header";
+import ListView from "./components/listview";
 
 const App: React.FC = () => {
     return(
         <>
             <Header />
-        </>);
+            <ListView />
+        </>
+    );
 };
 
 export default App;
---
+++ src/sidebar-react/components/listview/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ListView: React.FC = () => {
+    return (
+        <div className="content-area">
+        {/*
+        TODO 아래 컴포넌트 추가
+          <SortButton />
+          <KeywordItems />
+          */}
+        </div>
+    );
+};
+
+export default ListView;
