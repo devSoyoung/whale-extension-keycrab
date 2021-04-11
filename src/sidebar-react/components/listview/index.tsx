91d82f8d373229d3906715112cdbb18aaa8c54da
jincy7 <jincy713@gmail.com>
2021-04-11 22:10:48 +0900

    feat: 리스트뷰 설계 추가

--- src/sidebar-react/components/listview/index.tsx
+++ src/sidebar-react/components/listview/index.tsx
@@ -2,13 +2,25 @@ import React from 'react';
 
 const ListView: React.FC = () => {
   return (
-    <div className="content-area">
+    <main className="content-area">
       {/*
         TODO 아래 컴포넌트 추가
-          <SortButton />
-          <KeywordItems />
+          <Filter />
+          <Keywords>
+            <Keyword />
+              <FollowButton />
+              <div class="keyword-item-content">
+                타이틀 영역
+               <ShortCut />
+               <LinkList />
+                  <LinkItem />
+              </div>
+            </ Keyword>
+            <Keyword />
+            <Keyword />
+          <EmptyBox />
           */}
-    </div>
+    </main>
   );
 };
 
