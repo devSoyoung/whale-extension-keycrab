ed5625a18df80e60bd4b36bce2304dc24dbccbe5
jincy7 <jincy713@gmail.com>
2021-04-11 17:29:33 +0900

    feat: 타이틀, 검색창 영역 컴포넌트 추가

--- src/sidebar-react/App.tsx
+++ src/sidebar-react/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Title from "./components/Title";
+import SearchInput from "./components/SearchInput";
 
 const App: React.FC = () => {
-    return <div>Hello, react</div>;
+    return <div>
+        <Title/>
+        <SearchInput />
+    </div>;
 };
 
 export default App;
---
+++ src/sidebar-react/components/SearchInput.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SearchInput: React.FC = () => {
+    return (
+        <div className="search-input-area">
+            <img src="images/icons/search.png" alt="검색아이콘" className="search-icon" />
+                <input placeholder="검색할 키워드를 입력해주세요." className="search-input" type="text" />
+        </div>
+    )
+};
+
+export default SearchInput;
