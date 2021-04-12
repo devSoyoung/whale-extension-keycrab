c0324113f28dbce29fd11b2bd8c00f3fb1579d85
SoYoung Lee <soyoung.dev@gmail.com>
2021-04-12 09:15:05 +0900

    background - Message 타입 세분화

--- src/types/Message.ts
+++ src/types/Message.ts
@@ -1,9 +1,40 @@
-export interface MessagePayload {
+export interface KeywordPayload {
   keyword: string;
-  link?: string;
 }
 
-export interface Message {
-  type: string;
-  payload: MessagePayload;
+export interface LinkPayload {
+  keyword: string;
+  link: string;
 }
+
+interface FollowKeywordMessage {
+  type: 'FOLLOW_KEYWORD';
+  payload: KeywordPayload;
+}
+
+interface UnfollowKeywordMessage {
+  type: 'UNFOLLOW_KEYWORD';
+  payload: KeywordPayload;
+}
+
+interface RemoveKeywordMessage {
+  type: 'REMOVE_KEYWORD';
+  payload: KeywordPayload;
+}
+
+interface AddLinkMessage {
+  type: 'ADD_LINK_TO_KEYWORD';
+  payload: LinkPayload;
+}
+
+interface RemoveLinkMessage {
+  type: 'REMOVE_LINK';
+  payload: LinkPayload;
+}
+
+export type Message =
+  | FollowKeywordMessage
+  | UnfollowKeywordMessage
+  | RemoveKeywordMessage
+  | AddLinkMessage
+  | RemoveLinkMessage;
