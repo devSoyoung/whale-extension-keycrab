bf33bff48a30f4895065d0a10be2abb8882f5a50
jincy7 <jincy713@gmail.com>
2021-05-09 22:19:31 +0900

    feat: 링크 데이터 연동

--- src/sidebar-react/components/listview/Link.tsx
+++ src/sidebar-react/components/listview/Link.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import { Link as LinkProps } from '../../type/keywords';
 
+const OriginMap = {
+  total: '통합검색',
+  blog: '블로그',
+  cafe: '카페',
+  shopping: '쇼핑',
+  kin: '지식iN',
+  post: '포스트',
+  video: '동영상',
+  news: '뉴스',
+  website: '사이트',
+  google: '구글',
+};
+
+const DEFAULT_ORIGIN = '네이버';
+
+const PIN_IMAGE = 'images/icons/pin_fixed2.png';
+const UNPIN_IMAGE = 'images/icons/pin_unfixed.png';
+// 임시, x 표 아이콘 생성 필요
+const GARBAGE_IMAGE = 'images/icons/garbage.png';
+
 const Link = ({ favorite, origin, title, url }: LinkProps) => {
   return (
     <div className="card--main__link">
-      <button className="card--main__pin" />
-      <div className="card--main__origin">{origin}</div>
-      <div className="card--main__desc">{title}</div>
-      <button className="card--main__remove" />
+      <button className="card--main__pin">
+        <img src={favorite ? PIN_IMAGE : UNPIN_IMAGE} alt="고정하기" />
+      </button>
+      <div className="card--main__origin">
+        {OriginMap[origin] ?? DEFAULT_ORIGIN}
+      </div>
+      <div className="card--main__desc">
+        <a target="_blank" href={url} rel="noreferrer">
+          {title}
+        </a>
+      </div>
+      <button className="card--main__remove">
+        <img src={GARBAGE_IMAGE} alt="링크 삭제" />
+      </button>
     </div>
   );
 };
